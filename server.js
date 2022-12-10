c0cadec0024e0b5f80f3e9383f1f6cc287cf81d4
jack <=>
2022-12-10 22:46:10 +0300

    adding scrapping data

--- server.js
+++ server.js
@@ -9,9 +9,15 @@ app.use(cors());
 
 const user = require('./routes/users/users');
 const adminUser = require('./routes/admin/users');
-const sports = require('./routes/sports');
+const sports = require('./routes/sports/sports');
+const m_sports = require('./routes/sports/msports');
+
 const getMatches = require("./scrapers/getMatches");
 const getAllMatches = require("./scrapers/getAllMatches");
+const m_getMatches = require("./scrapers/m_getMatches");
+const m_getAllMatches = require("./scrapers/m_getAllMatches");
+const m_getTopLeagues = require("./scrapers/m_getTopLeagues");
+const m_getLeagueSorts = require("./scrapers/m_getLeagueSorts");
 
 //db connect
 const mongoose = require('mongoose')
@@ -22,6 +28,10 @@ mongoose.connect('mongodb://127.0.0.1:27017/sportBet', { useNewUrlParser: true }
 setInterval(() => {
     // getMatches();
     // getAllMatches();
+    // m_getMatches();
+    // m_getTopLeagues();
+    // m_getAllMatches();
+    // m_getLeagueSorts();
 }, 30000);
 
 // using middlewares
@@ -31,6 +41,7 @@ app.use(express.json())
 app.use('/user', user);
 app.use('/admin', adminUser);
 app.use('/sports', sports);
+app.use('/m_sports', m_sports);
 
 // listenig to port
 const port = 5000
