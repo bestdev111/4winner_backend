f2b6b20522d9f8a5eaec0ad993b8c37dc74e70c1
jack <=>
2022-12-19 16:10:17 +0300

    add result route

--- routes/sports/msports.js
+++ routes/sports/msports.js
@@ -61,5 +61,19 @@ router.get('/getLeagueSorts', async (req, res) => {
         res.status(500).json({ err });
     }
 });
+router.get('/getResult', async (req, res) => {
+    try {
+        console.log("hello:", req.body);
+        let date = '2022-12-19';
+        let betradarSportType = 1;
+        const url = `https://m.4winners.bet/Home/GetFinishedMatches?` +
+        `betradarSportType=${betradarSportType}&` +
+        `date=${date}`;
+        const { data } = await axios.get(url, { headers: { 'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Mobile Safari/537.36' } });
+        res.status(200).json({ data });
+    } catch (err) {
+        res.status(500).json({ err });
+    }
+});
 
 module.exports = router
